95eb1442e9d9dc18a6a24e35322f095c17a46cf3
fangbu <1581779395@qq.com>
2026-05-18 23:53:27 +0800

    nginx容器改造

--- client/src/App.vue
+++ client/src/App.vue
@@ -60,7 +60,7 @@ export default {
       this.message = ''
 
       try {
-        const response = await fetch('http://localhost:22048/api/users', {
+        const response = await fetch('/api/users', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json'
--- client/vite.config.js
+++ client/vite.config.js
@@ -11,5 +11,7 @@ export default defineConfig({
         changeOrigin: true
       }
     }
-  }
+  },
+  // 生产环境基础路径
+  base: '/'
 })
